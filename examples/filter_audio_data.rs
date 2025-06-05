f6992b793810d87361ab5f15125d584dd750fb8f
Tait Hoyem <tait@tait.tech>
2025-06-05 10:54:30 -0600

    logging

--- examples/filter_audio_data.rs
+++ examples/filter_audio_data.rs
@@ -21,13 +21,19 @@ fn main() {
         bits_per_sample: 16,
         sample_format: SampleFormat::Int,
     };
+    let mut offset = 0;
+    let mut diff = 0;
     let mut writer = WavWriter::create("out.wav", spec).expect("Can make wave writer!");
     for _ in 0..55 {
+        diff = data.len();
         (data_next, msg) = read_message(data, header).expect("to be able to read data");
+        diff -= data_next.len();
+        offset += diff;
+        println!("OFFSET: {} LEN: {}", offset, diff);
         header = true;
         if let MessageType::Audio { samples_offset, samples_len } = msg {
             let ch = &data[samples_offset..samples_len];
-            println!("DATA: {:?}", ch);
+            println!("FIRST 12 BYTES: {:?}", &ch[..12]);
             for (l,h) in ch.iter().tuples() {
                 let sample = i16::from_le_bytes([*l,*h]);
                 writer.write_sample(sample).expect("Can write to file");
